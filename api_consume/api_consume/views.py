669c12557cb719e9e5827ec113e61c5cb1e167f2
Nazmus Shakib <44675374+shakib500@users.noreply.github.com>
2019-05-12 15:13:19 +0600

    get request and fetch the data using api

---
+++ api_consume/api_consume/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+import requests
+
+def home(request):
+    response = requests.get('http://api.ipstack.com/203.89.125.5?access_key=28801c5ccfc535b3216e1f6c335022d9')
+    geodata = response.json()
+    return render(request, 'home.html', {
+        'ip': geodata['ip'],
+        'country': geodata['country_name'],
+        'city': geodata['city']
+    })
+
+    # return render(request, 'home.html')
