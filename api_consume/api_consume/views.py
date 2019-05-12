c65e4691dd2c9524bfb8b247fafac9168ddb08be
Nazmus Shakib <44675374+shakib500@users.noreply.github.com>
2019-05-12 15:15:31 +0600

    static ip address changed to dynamic

--- api_consume/api_consume/views.py
+++ api_consume/api_consume/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 import requests
 
 def home(request):
-    response = requests.get('http://api.ipstack.com/203.89.125.5?access_key=28801c5ccfc535b3216e1f6c335022d9')
+    response = requests.get('http://api.ipstack.com/check?access_key=28801c5ccfc535b3216e1f6c335022d9')
     geodata = response.json()
     return render(request, 'home.html', {
         'ip': geodata['ip'],
