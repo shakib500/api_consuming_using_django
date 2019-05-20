1888525340f06b299a3b0b8ad0d231a53d8954c8
Nazmus Shakib <44675374+shakib500@users.noreply.github.com>
2019-05-21 01:02:20 +0600

    github api endpoint added
    can search for a user name and get user name and public repo

--- api_consume/api_consume/urls.py
+++ api_consume/api_consume/urls.py
@@ -20,4 +20,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home, name='home'),
+    path('github/',views.github, name='github'),
 ]
--- api_consume/api_consume/views.py
+++ api_consume/api_consume/views.py
@@ -9,5 +9,11 @@ def home(request):
         'country': geodata['country_name'],
         'city': geodata['city']
     })
-
-    # return render(request, 'home.html')
+def github(request):
+    user = {}
+    if 'username' in request.GET:
+        username = request.GET['username']
+        url = 'https://api.github.com/users/%s' % username
+        response = requests.get(url)
+        user = response.json()
+    return render(request, 'github.html', {'user': user})    
